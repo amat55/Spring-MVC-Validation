caee3991908f44518bbff2baa3060f19a805ab2c
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-08-11 15:39:09 +0200

    Created Custom Messages properties

--- src/main/java/com/ahmetsenocak/mvc/Customer.java
+++ src/main/java/com/ahmetsenocak/mvc/Customer.java
@@ -12,9 +12,10 @@ public class Customer {
     @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 cars/digits")
     private String postalCode;
 
+    @NotNull(message = "is required")
     @Min(value = 0, message = "must be greater than 0 or equal to 0")
     @Max(value = 10, message = "must be less than 0 or equal to 10")
-    private int freePasses;
+    private Integer freePasses;
 
     public String getPostalCode() {
         return postalCode;
@@ -24,11 +25,11 @@ public class Customer {
         this.postalCode = postalCode;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
