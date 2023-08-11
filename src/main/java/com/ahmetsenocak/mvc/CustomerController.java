a23baec4cc8850f538fd01f17b999a696ecceabe
Ahmet Senocak <ahmetsaid_senocak@hotmail.com>
2023-08-11 13:26:04 +0200

    Created project, created basic form

---
+++ src/main/java/com/ahmetsenocak/mvc/CustomerController.java
@@ -0,0 +1,15 @@
+package com.ahmetsenocak.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+
+    @GetMapping("/")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("customer", new Customer());
+        return "customer-form";
+    }
+}
